8046d0d57fbba6871e03a20b000363e11ebf2ce2
thuongnv00 <ngo.van.thuong@vsi-international.com>
2022-03-05 11:04:09 +0700

    update 05/03 format date

--- src/main/java/com/example/appdemo/controller/EmployeeController.java
+++ src/main/java/com/example/appdemo/controller/EmployeeController.java
@@ -7,14 +7,9 @@ import com.example.appdemo.service.DepartmentService;
 import com.example.appdemo.service.EmployeeService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -30,13 +25,11 @@ public class EmployeeController {
     private final EmployeeService employeeService;
     private final DepartmentService departmentService;
 
-    private final FormatDate formatDate;
 
 
-    public EmployeeController(EmployeeService employeeService, DepartmentService departmentService, FormatDate formatDate) {
+    public EmployeeController(EmployeeService employeeService, DepartmentService departmentService) {
         this.employeeService = employeeService;
         this.departmentService = departmentService;
-        this.formatDate = formatDate;
     }
 
     @GetMapping("/all")
@@ -46,7 +39,7 @@ public class EmployeeController {
         List<EmployeeModel> employeeModels =  employees.stream().map(employee -> modelMapper.map(employee,EmployeeModel.class)).collect(Collectors.toList());
         for(EmployeeModel employeeModel:employeeModels) {
             for (Employee employee:employees) {
-                if(employeeModel.getId() == employee.getId()) {
+                if((employeeModel.getId() == employee.getId())& employee.getBirthDate()!= null) {
                     employeeModel.setBirthDate(employee.getBirthDate().format(formatter));
                 }
             }
@@ -58,35 +51,50 @@ public class EmployeeController {
     public EmployeeModel getEmployeesById(@PathVariable("id") Long id){
         Employee employee = employeeService.findEmployeeById(id);
         EmployeeModel employeeModel = modelMapper.map(employee,EmployeeModel.class);
-        employeeModel.setBirthDate(employee.getBirthDate().format(formatter));
+        if(employee.getBirthDate()!= null){
+            employeeModel.setBirthDate(employee.getBirthDate().format(formatter));
+        }
         return employeeModel;
     }
 
 
 
     @PutMapping ("/update")
-    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
-        Employee employeeUpdate = employeeService.updateEmployee(employee);
-        return new ResponseEntity<>(employeeUpdate,HttpStatus.OK);
+    public ResponseEntity<EmployeeModel> updateEmployee(@RequestBody EmployeeModel employeeModel) {
+        Employee request = modelMapper.map(employeeModel,Employee.class);
+        request.setBirthDate(LocalDate.parse(employeeModel.getBirthDate(),formatter));
+        Employee employee = employeeService.updateEmployee(request);
+        EmployeeModel response = modelMapper.map(employee,EmployeeModel.class);
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) throws IOException {
-            Employee newEmployee = employeeService.addEmployee(employee);
-            return new ResponseEntity<>(newEmployee,HttpStatus.CREATED);
+    public ResponseEntity<EmployeeModel> addEmployee(@RequestBody EmployeeModel employeeModel) {
+        Employee request = modelMapper.map(employeeModel,Employee.class);
+        request.setBirthDate(LocalDate.parse(employeeModel.getBirthDate(),formatter));
+        Employee employee = employeeService.addEmployee(request);
+        EmployeeModel response = modelMapper.map(employee,EmployeeModel.class);
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
        employeeService.deleteEmployee(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>("Delete employee successfully !",HttpStatus.OK);
     }
 
     @GetMapping("/department/{departmentId}/employees")
-    public ResponseEntity<List<Employee>> getAllEmployeesByDepartmentId(@PathVariable(value = "departmentId") Long id) {
-
+    public ResponseEntity<List<EmployeeModel>> getAllEmployeesByDepartmentId(@PathVariable(value = "departmentId") Long id) {
         List<Employee> employees = employeeService.findByDepartmentId(id);
-        return new ResponseEntity<>(employees, HttpStatus.OK);
+        List<EmployeeModel> employeeModels = employees.stream().map(employee -> modelMapper.map(employee,EmployeeModel.class)).collect(Collectors.toList());
+        for(EmployeeModel employeeModel:employeeModels) {
+            for(Employee employee : employees) {
+                if((employeeModel.getId() == employee.getId())& employee.getBirthDate()!= null) {
+                    employeeModel.setBirthDate(employee.getBirthDate().format(formatter));
+                }
+            }
+        }
+        return new ResponseEntity<>(employeeModels, HttpStatus.OK);
     }
 
 }
